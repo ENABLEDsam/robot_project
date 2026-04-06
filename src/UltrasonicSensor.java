80a5caef771a1a98f82986640616bc71720a572b
Valtteri Markus Ahde <vmahde13@gmail.com>
2026-04-06 13:01:09 +0300

    Added UltrasonicSensor.java file

---
+++ src/UltrasonicSensor.java
@@ -0,0 +1,20 @@
+package src;
+
+import lejos.hardware.Button;
+import lejos.hardware.SensorEV3UltrasonicSensor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+public class Ultrasonic Sensor {
+
+    public static void main(String[] args) {
+
+
+
+
+        
+    }
+}
