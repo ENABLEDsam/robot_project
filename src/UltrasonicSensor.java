d997590a9afe98cc89c2ba41b5868f59ca5a7359
Valtteri Markus Ahde <vmahde13@gmail.com>
2026-04-06 14:34:30 +0300

    Added US sensor
    Added code for functioning US sensor loop.
    Made tweaks & fixed typos

--- src/UltrasonicSensor.java
+++ src/UltrasonicSensor.java
@@ -1,20 +1,58 @@
 package src;
 
 import lejos.hardware.Button;
-import lejos.hardware.SensorEV3UltrasonicSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
-public class Ultrasonic Sensor {
+
+
+public class UltrasonicSensor {
 
     public static void main(String[] args) {
 
+        EV3UltrasonicSensor usSensor = new EV3UltrasonicSensor(SensorPort.S1);
+        SampleProvider distanceSample = usSensor.getDistanceMode();
+        float[] distanceData = new float[distanceSample.sampleSize()];
+
+        EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
+        EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+
+        int baseSpeed = 200;
+
+        // distance at which the robot stops 0.20f = 20 cm.
+        float stopDistance = 0.20f;
 
 
+        while (!Button.ESCAPE.isDown()) {
+            distanceSample.fetchSample(distanceData, 0);
+            float distance = distanceData[0];
 
-        
+                if (distance < stopDistance) {
+                    leftMotor.stop(true);
+                    rightMotor.stop();
+                }
+                else {
+                    leftMotor.setSpeed(baseSpeed);
+                    rightMotor.setSpeed(baseSpeed);
+                    leftMotor.forward();
+                    rightMotor.forward();
+                }
+
+                Delay.msDelay(50);
+        }
+
+        leftMotor.stop();
+        rightMotor.stop();
+        leftMotor.close();
+        rightMotor.close();
+        usSensor.close();
+
     }
+
+
+    
 }
